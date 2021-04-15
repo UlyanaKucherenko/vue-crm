dedc28215f29c1f690e9c59f58069a466c71e8ef
ulyana <ulyanakucherenko28@gmail.com>
2021-04-15 20:13:26 +0300

    created layouts & active btn nav

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: value}">
+    
+    <li 
+    v-for="link in links" 
+    :key="link.url">
+
+      <router-link
+      :key="link.url"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exect"
+      >
+        <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+ 
+  name: 'Sidebar',
+  props: ['value'],
+  data(){
+    return{
+      links: [ 
+          { title: 'Счет', url: '/', exect:true },
+          { title: 'История', url: '/history' },
+          { title: 'Планирование', url: '/planning' },
+          { title: 'Новая запись', url: '/record' },
+          { title: 'Категории', url: '/categories' }
+        ]
+    }
+  },
+}
+</script>
